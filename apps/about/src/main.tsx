7cc4d2e816b89769ec7d039f5f2e51bb5d329186
Nicholas Cunningham <nico@Nicholass-MacBook-Pro.local>
2022-03-11 10:13:07 -0700

    Add dynamic import for about boostrap

--- apps/about/src/main.tsx
+++ apps/about/src/main.tsx
@@ -1,11 +1,1 @@
-import { StrictMode } from 'react';
-import * as ReactDOM from 'react-dom';
-
-import App from './app/app';
-
-ReactDOM.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-  document.getElementById('root')
-);
+import ('./boostrap');
